b8039ffc5482475eb8949028646aea248311ea8c
Rohit Kumar <shutupRohit@gmail.com>
2021-09-22 23:57:29 +0530

    project section added

--- javascript/navScroll.js
+++ javascript/navScroll.js
@@ -1,5 +1,6 @@
 const header = document.querySelector('header');
 const sectionOne = document.querySelector('.home-intro');
+const mainNav = document.querySelector('.main-nav');
 
 const sectionOneOptions = {
     rootMargin: "-200px 0px 0px 0px"
@@ -8,8 +9,12 @@ const sectionOneObserver = new IntersectionObserver(function(entries, sectionOne
     entries.forEach(entry =>{
         if(!entry.isIntersecting){
             header.classList.add('nav-scrolled');
+            mainNav.classList.add('main-nav-scrolled');
         }
-        else header.classList.remove('nav-scrolled');
+        else {
+            header.classList.remove('nav-scrolled');
+            mainNav.classList.remove('main-nav-scrolled');
+        }
     });
 }, sectionOneOptions);
 
